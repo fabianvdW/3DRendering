8f2a208e9de129337d062574752965464f25dbcb
fabianvdW <fabianvonderwarth@gmail.com>
2020-02-29 14:34:07 +0100

    Hello World Rectangle

--- src/bin/demo.rs
+++ src/bin/demo.rs
@@ -5,8 +5,12 @@ extern crate sdl2;
 use lib::*;
 use sdl2::event::Event;
 use sdl2::keyboard::Scancode;
+use std::ffi::{c_void, CString};
 
 fn main() {
+    let vertex_shader_source = CString::new(load_file("shaders/vertex_shader.glsl")).unwrap();
+    let fragment_shader_source = CString::new(load_file("shaders/fragment_shader.glsl")).unwrap();
+
     let sdl = sdl2::init().unwrap();
     let mut event_pump = sdl.event_pump().unwrap();
     let video_subsystem = sdl.video().unwrap();
@@ -28,18 +32,52 @@ fn main() {
     //Set the viewport and color
     unsafe {
         gl::Viewport(0, 0, 900, 700);
-        gl::ClearColor(0.2, 0.3, 0.3, 1.0);
-        gl::Clear(gl::COLOR_BUFFER_BIT)
     }
 
-    let vertices = [(-0.5, -0.5, 0.), (0.5, -0.5, 0.), (0., 0.5, 0.)];
+    //Create shader
+    let vertex_shader = shader_from_source(&vertex_shader_source, gl::VERTEX_SHADER).unwrap();
+    let fragment_shader = shader_from_source(&fragment_shader_source, gl::FRAGMENT_SHADER).unwrap();
+    let shader_program = link_shaders(&[vertex_shader, fragment_shader]).unwrap();
 
+    //Create vertices
+    let vertices: [f32; 12] = [
+        0.5, 0.5, 0.0, 0.5, -0.5, 0.0, -0.5, -0.5, 0.0, -0.5, 0.5, 0.0,
+    ];
+    let indices: [u32; 6] = [0, 1, 3, 1, 2, 3];
+
+    let mut vao = 0;
     let mut vbo = 0;
+    let mut ebo = 0;
     unsafe {
-        gl::GenBuffers(2, &mut vbo);
+        gl::GenVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
+
+        gl::GenBuffers(1, &mut vbo);
         gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            4 * vertices.len() as isize,
+            vertices.as_ptr() as *const c_void,
+            gl::STATIC_DRAW,
+        );
+
+        gl::GenBuffers(1, &mut ebo);
+        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+        gl::BufferData(
+            gl::ELEMENT_ARRAY_BUFFER,
+            4 * indices.len() as isize,
+            indices.as_ptr() as *const c_void,
+            gl::STATIC_DRAW,
+        );
+
+        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 3 * 4, std::ptr::null());
+        gl::EnableVertexAttribArray(0);
+
+        gl::BindVertexArray(0);
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, 0);
     }
-    println!("{}", vbo);
+    let mut wireframe = false;
     'main: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -48,10 +86,34 @@ fn main() {
                     scancode: Some(Scancode::Escape),
                     ..
                 } => break 'main,
+                Event::KeyDown {
+                    scancode: Some(Scancode::Space),
+                    ..
+                } => {
+                    wireframe = !wireframe;
+                    unsafe {
+                        gl::PolygonMode(
+                            gl::FRONT_AND_BACK,
+                            if wireframe { gl::LINE } else { gl::FILL },
+                        );
+                    }
+                }
                 _ => {
                     housekeeping(event);
                 }
             }
+        }
+        unsafe {
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+            gl::UseProgram(shader_program);
+            gl::BindVertexArray(vao);
+            gl::DrawElements(
+                gl::TRIANGLES,
+                indices.len() as i32,
+                gl::UNSIGNED_INT,
+                std::ptr::null(),
+            );
         }
 
         window.gl_swap_window();
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use sdl2::event::EventType::Window;
+use gl::types::*;
 use sdl2::event::{Event, WindowEvent};
 use std::ffi::{CStr, CString};
 
@@ -12,31 +12,49 @@ pub fn housekeeping(event: Event) {
         }
     }
 }
-
-pub fn shader_from_source(
-    source: &CStr,
-    kind: gl::types::GLenum,
-) -> Result<gl::types::GLuint, String> {
+pub fn load_file(path: &str) -> String {
+    let contents = std::fs::read_to_string(path);
+    match contents {
+        Ok(s) => s,
+        Err(e) => panic!("{}", e.to_string()),
+    }
+}
+pub fn link_shaders(shaders: &[GLuint]) -> Result<GLuint, String> {
+    unsafe {
+        let id = gl::CreateProgram();
+        for shader in shaders {
+            gl::AttachShader(id, *shader);
+        }
+        gl::LinkProgram(id);
+        let mut success = 1;
+        gl::GetProgramiv(id, gl::LINK_STATUS, &mut success);
+        if success == 0 {
+            let mut len = 0;
+            gl::GetProgramiv(id, gl::INFO_LOG_LENGTH, &mut len);
+            let buffer = vec![0u8; len as usize];
+            gl::GetProgramInfoLog(id, len, std::ptr::null_mut(), buffer.as_ptr() as *mut i8);
+            Err(CString::from_vec_unchecked(buffer).into_string().unwrap())
+        } else {
+            for shader in shaders {
+                gl::DeleteShader(*shader);
+            }
+            Ok(id)
+        }
+    }
+}
+pub fn shader_from_source(source: &CStr, kind: GLenum) -> Result<GLuint, String> {
     unsafe {
         let id = gl::CreateShader(kind);
         gl::ShaderSource(id, 1, &source.as_ptr(), std::ptr::null());
         gl::CompileShader(id);
         let mut success: gl::types::GLint = 1;
         gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
-
         if success == 0 {
             let mut len: gl::types::GLint = 0;
             gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
-            let mut buffer: Vec<u8> = Vec::with_capacity(len as usize + 1);
-            buffer.extend([b' '].iter().cycle().take(len as usize));
-            let error: CString = CString::from_vec_unchecked(buffer);
-            gl::GetShaderInfoLog(
-                id,
-                len,
-                std::ptr::null_mut(),
-                error.as_ptr() as *mut gl::types::GLchar,
-            );
-            Err(error.to_string_lossy().into_owned())
+            let buffer = vec![0u8; len as usize];
+            gl::GetShaderInfoLog(id, len, std::ptr::null_mut(), buffer.as_ptr() as *mut i8);
+            Err(CString::from_vec_unchecked(buffer).into_string().unwrap())
         } else {
             Ok(id)
         }
